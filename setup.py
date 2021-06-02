00cd8a286caa04b3bf29d8db403ecacc4e64be61
sscottgvit <sscott@govanguard.com>
2021-06-02 13:33:25 -0500

    Fix dep typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyShodan",
-    version="0.2.4",
+    version="0.2.5",
     author="Shane Scott",
     author_email="sscott@gvit.com",
     description="Python library for querying the Shodan API",
