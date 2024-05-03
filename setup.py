4ef58f3827aa9c71d59d2928b6b0d1502f724349
GitHub Action <action@github.com>
2024-05-03 16:02:01 +0000

    Bump version to 0.2.9 and update ChangeLog

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyShodan",
-    version="0.2.8",
+    version="0.2.9",
     author="Shane Scott",
     author_email="sscott@gotham-security.com",
     description="Python library for querying the Shodan API",
